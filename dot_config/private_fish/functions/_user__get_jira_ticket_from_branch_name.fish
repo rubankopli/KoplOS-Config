d97925c0dafe80d0edd1b5ce6367a82016f2d5ac
Ruban Kopli <rubankopli@gmail.com>
2025-12-28 23:36:52 -0500

    Removed fish files for plugins, which should be generated when they are installed via fisher

--- dot_config/private_fish/functions/_user__get_jira_ticket_from_branch_name.fish
+++
@@ -1,7 +0,0 @@
-# ? NOTE: Must be run from within a git repository!
-
-function _get_jira_ticket_from_branch_name --description 'Parse the current git branch name and attempt to determine the relevant jira ticket its used with'
-    set branch_name (git rev-parse --abbrev-ref HEAD)
-    set ticket_name (string match --regex "[[:upper:]]{2,10}.[[:digit:]]{1,5}" $branch_name)
-    echo $ticket_name
-end
